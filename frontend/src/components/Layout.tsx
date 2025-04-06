c0872897602896820837ce3757de54e9c01a7f48
Krishaay Jois <krishaayjois@gmail.com>
2025-04-06 08:50:15 +0530

    Fixed UI issue

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -90,7 +90,7 @@ const Layout = () => {
                 : 'text-gray-400 hover:text-green-400 hover:bg-green-900/10'
             }`}
           >
-            <div className="h-5 w-5 flex items-center justify-center">
+            <div className="h-5 w-5 flex items-center justify-center mr-3">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 8V16C21 18.7614 18.7614 21 16 21H8C5.23858 21 3 18.7614 3 16V8C3 5.23858 5.23858 3 8 3H16C18.7614 3 21 5.23858 21 8Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M9 13.5V9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
